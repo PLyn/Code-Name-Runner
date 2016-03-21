75d8a662e3532449485b641a517aab8bb0e58a2e
PLyn <plyn8750@gmail.com>
2016-03-21 05:33:01 -0500

    Set Current level when new terrain is generated

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -2,11 +2,12 @@
 using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour {
-    public static string CurrentStageName;
+    public static string CurrentStageName = "Stage1_1";
     private PlayerController PlayerScript;
     public GameObject NewStage;
     private GameObject OldStage;
     private float CurrentStageWidth;
+    private int NewStageIndex;
     private int CurrentStageIndex;
     private int StartStageIndex;
     private int EndStageIndex;
@@ -14,13 +15,15 @@ public class TerrainGenerator : MonoBehaviour {
     void Start()
     {
         //find current stage and determine how much levels exists for this stage
-        FindNumberOfLevels();
+        //FindNumberOfLevels();
         PlayerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
     public void GenerateNewTerrain()
     {
         OldStage = GameObject.Find("Ground");
-        NewStage = (GameObject)Instantiate(Resources.Load("Stage" + "1" + "_" + UnityEngine.Random.Range(StartStageIndex, EndStageIndex)));
+        NewStageIndex = UnityEngine.Random.Range(StartStageIndex, EndStageIndex);
+        NewStage = (GameObject)Instantiate(Resources.Load("Stage" + "1" + "_" +  NewStageIndex));
+        CurrentStageName = "Stage" + "1" + "_" + NewStageIndex;
 
         CurrentStageWidth = PlayerScript.CurrentStage.transform.Find("Ground").gameObject.GetComponent<Collider2D>().bounds.size.x;
         NewStage.transform.position = new Vector3(CurrentStageWidth - StageController.CurrentSpeed, 
@@ -34,7 +37,7 @@ public class TerrainGenerator : MonoBehaviour {
         //search for how much files or prefabs exist for this level - eg there is 2 pre fabs for stage 1_x right now
 
         //set start, current and end indexes based on prefabs found
-        CurrentStageIndex = Int32.Parse(CurrentStageName.Substring(7, 1));
+       
         StartStageIndex = 1;
         EndStageIndex = 3;
 }
