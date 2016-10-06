2fd0e0409832887780fae6b21a7c86d43c61e1b3
PLyn <plyn8750@gmail.com>
2016-10-05 20:13:24 -0500

    - Organized scripts into folders for maintainability sake - Updated Collectible to Coin for now as a template for future Collectibles - Fixed the logic for the Return to Start on the Game over Screen - Fixed the logic for the Start screen so the gameplay loop is complete.

--- Assets/Scripts/Collectibles/Coin.cs
+++ Assets/Scripts/Collectibles/Coin.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Collectible : MonoBehaviour {
+public class Coin : MonoBehaviour {
+    public static int TotalCoinValue;
+    private int DashIndex;
+
     void Start()
     {
     }
@@ -15,5 +18,8 @@ public class Collectible : MonoBehaviour {
     {
         print("Collision Occured - collectible");
         this.GetComponent<SpriteRenderer>().enabled = false;
+
+        DashIndex = this.tag.IndexOf("-") + 1;
+        TotalCoinValue += int.Parse(this.tag.Substring(DashIndex));
     }
 }
